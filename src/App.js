86b6661aeac747bbe58e4886a30bf326a2f033f2
Julian Vos <post@julianvos.nl>
2022-05-30 11:04:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {
+    Avatar, Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Collapse, Grid,
+    IconButton, List, ListItem, ListItemAvatar, ListItemText,
+    styled,
+    Typography
+} from "@mui/material";
+import {ExpandMore as ExpandMoreIcon} from "@mui/icons-material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [roomStats, setRoomStats] = useState(null);
+    const [expanded, setExpanded] = useState({});
+
+    const ExpandMore = styled((props) => {
+        const {expand, ...other} = props;
+        return <IconButton {...other} />;
+    })(({theme, expand}) => ({
+        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    }));
+
+    useEffect(() => {
+        populateRoomStats();
+    }, []);
+
+    const handleExpandClick = (event) => {
+        const key = event.target.closest(".roomCard").attributes["data-key"].nodeValue;
+        const newExpanded = {...expanded};
+
+        if (newExpanded[key] === undefined) {
+            newExpanded[key] = true;
+        } else {
+            newExpanded[key] = !newExpanded[key];
+        }
+
+        setExpanded(newExpanded);
+    }
+
+    const populateRoomStats = async () => {
+        const response = await fetch("https://jqbx.fm/active-rooms/0");
+        const json = await response.json();
+
+        setRoomStats(json);
+    }
+
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2}>
+                {roomStats !== null && roomStats.rooms.map((room) => (
+                    <Grid item xl={2} lg={3} md={4} xs={6}>
+                        <Card className="roomCard" key={room._id} data-key={room._id} sx={{maxWidth: 345}}>
+                            <CardHeader title={room.title} subheader={room.genre}/>
+                            {room.tracks.length > 0 && (
+                                <>
+                                    <CardMedia component="img" image={room.tracks[0].album.images[0].url}/>
+                                    <CardContent>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {room.tracks[0].artists[0].name + " - " + room.tracks[0].album.name}
+                                        </Typography>
+                                    </CardContent>
+                                </>
+                            )}
+                            <CardActions disableSpacing>
+                                <Typography>Users ({room.usersCount})</Typography>
+                                <ExpandMore expand={expanded[room._id]} onClick={handleExpandClick}
+                                            aria-expanded={expanded[room._id]} aria-label="Show Users">
+                                    <ExpandMoreIcon/>
+                                </ExpandMore>
+                            </CardActions>
+                            <Collapse in={expanded[room._id]} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <List>
+                                        {room.users.map((user) => (
+                                            <ListItem key={user._id}>
+                                                <ListItemAvatar>
+                                                    <Avatar alt={user.username} src={user.image}/>
+                                                </ListItemAvatar>
+                                                <ListItemText primary={user.username}/>
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </CardContent>
+                            </Collapse>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
 }
 
 export default App;
