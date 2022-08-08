635ad3cc30b2f207fcf705ce6798f3e2d0d939f4
Julian Vos <post@julianvos.nl>
2022-08-08 13:52:53 +0200

    Use masonry instead of grid

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ import {
     Typography
 } from "@mui/material";
 import {ExpandMore as ExpandMoreIcon} from "@mui/icons-material";
+import * as PropTypes from "prop-types";
+
+function Masonry(props) {
+    return null;
+}
+
+Masonry.propTypes = {
+    container: PropTypes.bool,
+    spacing: PropTypes.number,
+    children: PropTypes.node
+};
 
 function App() {
     const [roomStats, setRoomStats] = useState(null);
@@ -53,48 +64,44 @@ function App() {
     }
 
     return (
-        <Box sx={{flexGrow: 1}}>
-            <Grid container spacing={2}>
-                {roomStats !== null && roomStats.rooms.map((room) => (
-                    <Grid item xl={2} lg={3} md={4} xs={6}>
-                        <Card className="roomCard" key={room._id} data-key={room._id} sx={{maxWidth: 345}}>
-                            <CardHeader title={room.title} subheader={room.genre}/>
-                            {room.tracks.length > 0 && (
-                                <>
-                                    <CardMedia component="img" image={room.tracks[0].album.images[0].url}/>
-                                    <CardContent>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {room.tracks[0].artists[0].name + " - " + room.tracks[0].album.name}
-                                        </Typography>
-                                    </CardContent>
-                                </>
-                            )}
-                            <CardActions disableSpacing>
-                                <Typography>Users ({room.usersCount})</Typography>
-                                <ExpandMore expand={expanded[room._id]} onClick={handleExpandClick}
-                                            aria-expanded={expanded[room._id]} aria-label="Show Users">
-                                    <ExpandMoreIcon/>
-                                </ExpandMore>
-                            </CardActions>
-                            <Collapse in={expanded[room._id]} timeout="auto" unmountOnExit>
-                                <CardContent>
-                                    <List>
-                                        {room.users.map((user) => (
-                                            <ListItem key={user._id}>
-                                                <ListItemAvatar>
-                                                    <Avatar alt={user.username} src={user.image}/>
-                                                </ListItemAvatar>
-                                                <ListItemText primary={user.username}/>
-                                            </ListItem>
-                                        ))}
-                                    </List>
-                                </CardContent>
-                            </Collapse>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
+        <Masonry columns={{ xs: 2, md: 3, lg: 4, xl: 6}}>
+            {roomStats !== null && roomStats.rooms.map((room) => (
+                <Card className="roomCard" key={room._id} data-key={room._id}>
+                    <CardHeader title={room.title} subheader={room.genre}/>
+                    {room.tracks.length > 0 && (
+                        <>
+                            <CardMedia component="img" image={room.tracks[0].album.images[0].url}/>
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    {room.tracks[0].artists[0].name + " - " + room.tracks[0].album.name}
+                                </Typography>
+                            </CardContent>
+                        </>
+                    )}
+                    <CardActions disableSpacing>
+                        <Typography>Users ({room.usersCount})</Typography>
+                        <ExpandMore expand={expanded[room._id]} onClick={handleExpandClick}
+                                    aria-expanded={expanded[room._id]} aria-label="Show Users">
+                            <ExpandMoreIcon/>
+                        </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded[room._id]} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <List>
+                                {room.users.map((user) => (
+                                    <ListItem key={user._id}>
+                                        <ListItemAvatar>
+                                            <Avatar alt={user.username} src={user.image}/>
+                                        </ListItemAvatar>
+                                        <ListItemText primary={user.username}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            ))}
+        </Masonry>
     );
 }
 
